23eaa3def36050991cd6eea21442ee5c5e2277f9
amestres <amestreszk@gmail.com>
2018-11-19 19:50:33 +0100

    adri clonado
    tamo bien

---
+++ resources/views/layouts/login.php
@@ -0,0 +1,10 @@
+<?php
+
+$nom = $_GET['nom'];
+$data = $_GET['data'];
+
+echo "<h2> Informació rebuda </h2>";
+echo "El nom és: " . $nom . "<br/>";
+echo "El data de neixement és: " . $data . "<br/>";
+
+?>
